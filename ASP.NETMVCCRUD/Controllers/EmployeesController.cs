506d64714e1f9d3d6afa23e30f318dfc81c18738
Marcello Cavazza <63473199+MarcelloCavazza@users.noreply.github.com>
2023-03-15 00:50:01 -0300

    criando controller de employees

---
+++ ASP.NETMVCCRUD/Controllers/EmployeesController.cs
@@ -0,0 +1,41 @@
+﻿using ASP.NETMVCCRUD.Data;
+using ASP.NETMVCCRUD.Models.Domain;
+using ASP.NETMVCCRUD.Models.View;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NETMVCCRUD.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly MyDbContext myDbContext;
+
+        public EmployeesController(MyDbContext myDbContext)
+        {
+            this.myDbContext = myDbContext;
+        }
+
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(AddEmployeeViewModel addEmployeeRequest)
+        {
+            Employees employees = new Employees()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmployeeRequest.Name,
+                Email = addEmployeeRequest.Email,
+                Password = addEmployeeRequest.Password,
+                Salary = addEmployeeRequest.Salary,
+            };
+
+            await myDbContext.Employees.AddAsync(employees);
+            await myDbContext.SaveChangesAsync();
+            return RedirectToAction("Add");
+
+        }
+    }
+}
