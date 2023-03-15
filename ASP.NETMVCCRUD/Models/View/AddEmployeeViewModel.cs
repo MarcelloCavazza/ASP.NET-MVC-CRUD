402c78d069dd722cc1952f599130c5fcc90ae5b1
Marcello Cavazza <63473199+MarcelloCavazza@users.noreply.github.com>
2023-03-15 00:50:52 -0300

    created for views model

---
+++ ASP.NETMVCCRUD/Models/View/AddEmployeeViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ASP.NETMVCCRUD.Models.View
+{
+    public class AddEmployeeViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public long Salary { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
