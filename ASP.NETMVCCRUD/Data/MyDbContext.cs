4eca2da86a70b1e6bafaf0a16a694d9b5ed63ff3
Marcello Cavazza <63473199+MarcelloCavazza@users.noreply.github.com>
2023-03-15 00:01:43 -0300

    Add project files.

---
+++ ASP.NETMVCCRUD/Data/MyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ASP.NETMVCCRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP.NETMVCCRUD.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+        }
+
+
+        public DbSet<Employees> Employees { get; set; }
+    }
+}
---
+++ ASP.NETMVCCRUD/Models/EmployeesModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ASP.NETMVCCRUD.Models
+{
+    [Table("Employees")]
+    public class Employees
+    {
+        [Display(Name = "Id")]
+        [Column("Id")]
+        public Guid Id { get; set; }
+        [Display(Name = "Name")]
+        [Column("Name")]
+        public string Name { get; set; }
+        [Display(Name = "Email")]
+        [Column("Email")]
+        public string Email { get; set; }
+        [Display(Name = "Password")]
+        [Column("Password")]
+        public string Password { get; set; }
+        [Display(Name = "Salary")]
+        [Column("Salary")]
+        public long Salary { get; set; }
+        [Display(Name = "CreatedAt")]
+        [Column("CreatedAt")]
+        public DateTime CreatedAt { get; set; }
+        [Display(Name = "UpdatedAt")]
+        [Column("UpdatedAt")]
+        public DateTime UpdatedAt { get; set; }
+    }
+}
